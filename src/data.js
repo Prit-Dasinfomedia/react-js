5ce92ece98e1a962b761c1f58236db48aef99947
Prit Patel <89439643+prit-dasinfomedia@users.noreply.github.com>
2021-11-25 09:55:31 +0530

    update

---
+++ src/data.js
@@ -0,0 +1,14 @@
+export const data =[
+    {
+        id: "1",
+        name: "prit",
+    },
+    {
+        id: "2",
+        name: "Harshil",
+    },
+    {
+        id: "3",
+        name: "harsh",
+    }
+]
